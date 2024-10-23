5f7c9610d0638fed190718bb13c643f5525231fa
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2024-10-23 13:21:58 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function validate() {
+  const inputCard = document.querySelector("#inputCard");
+  const inputCvc = document.querySelector("#inputCvc");
+  const inputAmount = document.querySelector("#inputAmount");
+  const inputFirstName = document.querySelector("#inputFirstName");
+  const inputLastName = document.querySelector("#inputLastName");
+  const inputCity = document.querySelector("#inputCity");
+  const inputState = document.querySelector("#inputState");
+  const inputPostalcode = document.querySelector("#inputPostalcode");
+  const inputCctype = document.querySelector("#inputCctype");
+  const inputMessage = document.querySelector("#inputMessage");
+  const form = document.querySelector("#form");
+  const errorElement = document.querySelector("#error");
+
+  document.querySelector("#alert").classList.add("hidden");
+  document.querySelector("#alert").innerHTML = "";
+
+  let messages = [];
+
+  if (inputCard.value.length < 10) {
+    messages += `<li>Input a valid Credit Card Number</li>`;
+  }
+  if (inputCvc.value.length > 3) {
+    messages += `<li>Wrong CVC number</li>`;
+  }
+
+  if (messages.length >= 3) {
+    document.querySelector("#alert").innerHTML = `<ul>${messages}</ul>`;
+    document.querySelector("#alert").classList.remove("hidden");
+  }
+}
+
+document.querySelector("#sendButton").addEventListener("click", e => {
+  e.preventDefault();
+  validate();
+});
